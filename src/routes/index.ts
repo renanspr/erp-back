c4b94af2936c13225d32c0f82b807ff111a2d3f1
Renan Spera <renansperacampos@hotmail.com>
2023-11-05 02:07:49 -0300

    docs: add swagger ui

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,22 @@
 import { Router } from 'express'
+import swaggerUi from 'swagger-ui-express'
 import { createUserController } from '@controllers/user'
+import swaggerDocs from '../../swagger.json'
 
 const router = Router()
+
+// Home
+router.get('/', (request, response) => {
+  response.send('Server is up :)')
+})
 
 // User routes
 router.post('/api/users', (request, response, next) => {
   createUserController(request, response, next)
 })
 
-router.get('/', (request, response) => {
-  response.send('Server is up :)')
-})
+// Docs
+router.use('/api-docs', swaggerUi.serve)
+router.get('/api-docs', swaggerUi.setup(swaggerDocs))
 
 export default router
