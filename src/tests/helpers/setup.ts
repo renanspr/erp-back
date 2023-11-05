8f31535de7de9d06220c634a5fe76048e18a5a40
Renan Spera <renansperacampos@hotmail.com>
2023-11-05 02:11:35 -0300

    test: reset db after run all tests

--- src/tests/helpers/setup.ts
+++ src/tests/helpers/setup.ts
@@ -1,6 +1,10 @@
-import { beforeEach } from 'vitest'
+import { beforeEach, afterAll } from 'vitest'
 import { resetDB } from './reset-db'
 
 beforeEach(async () => {
+  await resetDB()
+})
+
+afterAll(async () => {
   await resetDB()
 })
